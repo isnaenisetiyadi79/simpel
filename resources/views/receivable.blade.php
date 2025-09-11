3794e06fe43fe3c1bc782eae8c0f2a9ed5ccc485
isnaenisetiyadi79 <isnaenisetiyadi@gmail.com>
2025-09-11 09:58:11 +0800

    menghilangkan widget gaji di form hutang piutang

--- resources/views/receivable.blade.php
+++ resources/views/receivable.blade.php
@@ -6,12 +6,12 @@
 @section('content')
     <!-- Content -->
     <div class="w-full lg:ps-64">
-        <div class="p-4 sm:p-6 space-y-4 sm:space-y-6">
+        <div class="p-4 space-y-4 sm:p-6 sm:space-y-6">
 
 
 
             @livewire('components.order.widget')
-            @livewire('components.salary.widget')
+            {{-- @livewire('components.salary.widget') --}}
             @livewire('components.receivable.table')
         </div>
     </div>
